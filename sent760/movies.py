018d29f1d480815ffed25a70e13ae1a19f0c52da
Davis Allen <davis.b.allen@gmail.com>
2018-05-17 18:21:16 -0400

    Initial Commit

---
+++ sent760/movies.py
@@ -0,0 +1,249 @@
+from sklearn.datasets import load_files
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+import sklearn
+
+from keras.layers import Embedding
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+from keras.layers import Bidirectional
+
+import datetime
+
+# fix random seed for reproducibility
+np.random.seed(7)
+
+max_words = 20000
+
+moviedirtrain = r'/Users/davisallen/Education/Hunter/Classes/CSCI 760 - Computer Linguistics/Projects/SentiProject/Data/ImdbLargeMovieData/aclImdb/train'
+moviedirtest = r'/Users/davisallen/Education/Hunter/Classes/CSCI 760 - Computer Linguistics/Projects/SentiProject/Data/ImdbLargeMovieData/aclImdb/test'
+
+print(datetime.datetime.now())
+
+print("Loading IMDB data")
+movie_train = load_files(moviedirtrain, shuffle=True)
+movie_test = load_files(moviedirtest, shuffle=True)
+print(datetime.datetime.now())
+
+reviews_list = movie_train.data + movie_test.data
+class_array = np.concatenate((movie_train.target,movie_test.target), axis=0)
+# reviews_list = movie_train.data
+# class_array = movie_train.target
+
+
+
+
+
+
+print(datetime.datetime.now())
+
+print("Tokenizing and counting")
+# Create matrix of size num_reviews x num_tokens with counts for each word
+movie_vec = CountVectorizer(min_df=2, tokenizer=nltk.word_tokenize)         # use all 25K words. 82.2% acc.
+movie_counts = movie_vec.fit_transform(reviews_list)
+
+# Create TF IDF matrix
+tfidf_transformer = TfidfTransformer()
+movie_tfidf = tfidf_transformer.fit_transform(movie_counts)
+
+
+
+
+print(datetime.datetime.now())
+
+print("Tokenizing and indexing")
+# only use the top 20000 most commonly occurring words
+tokenizer = Tokenizer(nb_words=20000)
+# tokenize the reviews and generate reviews that are lists of indices for tokens in the dictionary
+tokenizer.fit_on_texts(reviews_list)
+sequences = tokenizer.texts_to_sequences(reviews_list)
+# the dictionary of tokens to indices
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+data = pad_sequences(sequences, maxlen=500)
+labels = class_array
+
+
+# nb_validation_samples = 25000
+nb_validation_samples = int(0.15 * data.shape[0])
+
+deep_x_train = data[:-nb_validation_samples]
+deep_x_test = data[-nb_validation_samples:]
+
+counts_x_train = movie_counts[:-nb_validation_samples]
+counts_x_test = movie_counts[-nb_validation_samples:]
+
+tfidf_x_train = movie_tfidf[:-nb_validation_samples]
+tfidf_x_test = movie_tfidf[-nb_validation_samples:]
+
+y_train = labels[:-nb_validation_samples]
+y_test = labels[-nb_validation_samples:]
+
+
+
+
+print(datetime.datetime.now())
+
+print("\n\n")
+
+print("Training NB classifier with counts")
+nb_model1 = MultinomialNB().fit(counts_x_train, y_train)
+# Predicting the Test set results, find accuracy
+y_pred1 = nb_model1.predict(counts_x_test)
+print("Accuracy: %.2f%%" % (sklearn.metrics.accuracy_score(y_test, y_pred1)*100))
+print("\n\n")
+
+print(datetime.datetime.now())
+
+print("Training NB classifier with TF-IDF")
+nb_model2 = MultinomialNB().fit(tfidf_x_train, y_train)
+# Predicting the Test set results, find accuracy
+y_pred2 = nb_model1.predict(tfidf_x_test)
+print("Accuracy: %.2f%%" % (sklearn.metrics.accuracy_score(y_test, y_pred2)*100))
+print("\n\n")
+
+
+
+# print(datetime.datetime.now())
+#
+# embedding_vector_length = 300
+# print("Building Bi-Directional LSTM using fasttext pretrained vectors")
+# print("Vector length is 300")
+# print("Word vectors can be modified during training")
+# model4 = Sequential()
+# # model4.add(Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], input_length=500))
+# model4.add(Embedding(len(word_index) + 1, embedding_vector_length, input_length=500))
+# model4.add(Bidirectional(LSTM(100), merge_mode='concat'))
+# model4.add(Dense(1, activation='sigmoid'))
+# model4.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# print(model4.summary())
+# model4.fit(deep_x_train, y_train, nb_epoch=3, batch_size=64)
+# # Final evaluation of the model
+# scores4 = model4.evaluate(deep_x_test, y_test, verbose=0)
+# print("Accuracy: %.2f%%" % (scores4[1]*100))
+# print("\n\n")
+
+
+
+
+
+# print(datetime.datetime.now())
+#
+# print("Building LSTM with no initial embedding vector weights")
+# print("Vector length is 32")
+# embedding_vector_length = 32
+# model0 = Sequential()
+# model0.add(Embedding(len(word_index) + 1, embedding_vector_length, input_length=500))
+# model0.add(LSTM(100))
+# model0.add(Dense(1, activation='sigmoid'))
+# model0.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# print(model0.summary())
+# model0.fit(deep_x_train, y_train, nb_epoch=3, batch_size=64)
+# # Final evaluation of the model
+# scores0 = model0.evaluate(deep_x_test, y_test, verbose=0)
+# print("Accuracy: %.2f%%" % (scores0[1]*100))
+# print("\n\n")
+#
+#
+#
+print(datetime.datetime.now())
+
+print("Building LSTM with no initial embedding vector weights")
+print("Vector length is 300")
+embedding_vector_length = 300
+model1 = Sequential()
+model1.add(Embedding(len(word_index) + 1, embedding_vector_length, input_length=500))
+model1.add(LSTM(100))
+model1.add(Dense(1, activation='sigmoid'))
+model1.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model1.summary())
+model1.fit(deep_x_train, y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores1 = model1.evaluate(deep_x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores1[1]*100))
+print("\n\n")
+
+
+
+
+
+print(datetime.datetime.now())
+
+print("Loading fasttext pretrained word vectors")
+print("This may take some time")
+from gensim.models import FastText
+from gensim.models import KeyedVectors
+# en_model = FastText.load_fasttext_format('/Users/davisallen/Education/Hunter/Classes/CSCI 760 - Computer Linguistics/Projects/SentiProject/Data/fasttextVectors/wiki.en/wiki.en')
+en_model = KeyedVectors.load_word2vec_format('/Users/davisallen/Education/Hunter/Classes/CSCI 760 - Computer Linguistics/Projects/SentiProject/Data/fasttextVectors/wiki-news-300d-1M.vec')
+
+embedding_matrix = np.zeros((len(word_index) + 1, 300))
+for word, i in word_index.items():
+    if word in en_model:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = en_model[word]
+
+
+print(datetime.datetime.now())
+
+print("Building LSTM using fasttext pretrained vectors")
+print("Vector length is 300")
+print("We are keeping the word vectors static and not allowing them to be modified during training")
+model2 = Sequential()
+model2.add(Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], input_length=500, trainable=False))
+model2.add(LSTM(100))
+model2.add(Dense(1, activation='sigmoid'))
+model2.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model2.summary())
+model2.fit(deep_x_train, y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores2 = model2.evaluate(deep_x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores2[1]*100))
+print("\n\n")
+
+print(datetime.datetime.now())
+
+print("Building LSTM using fasttext pretrained vectors")
+print("Vector length is 300")
+print("Word vectors can be modified during training")
+model3 = Sequential()
+model3.add(Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], input_length=500))
+model3.add(LSTM(100))
+model3.add(Dense(1, activation='sigmoid'))
+model3.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model3.summary())
+model3.fit(deep_x_train, y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores3 = model3.evaluate(deep_x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores3[1]*100))
+print("\n\n")
+
+
+
+print(datetime.datetime.now())
+
+embedding_vector_length = 300
+print("Building Bi-Directional LSTM using fasttext pretrained vectors")
+print("Vector length is 300")
+print("Word vectors can be modified during training")
+model4 = Sequential()
+model4.add(Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], input_length=500))
+# model4.add(Embedding(len(word_index) + 1, embedding_vector_length, input_length=500))
+model4.add(Bidirectional(LSTM(100), merge_mode='concat'))
+model4.add(Dense(1, activation='sigmoid'))
+model4.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model4.summary())
+model4.fit(deep_x_train, y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores4 = model4.evaluate(deep_x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores4[1]*100))
+print("\n\n")
+
---
+++ sent760/semeval.py
@@ -0,0 +1,292 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import datetime
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+import sklearn
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from keras.utils import np_utils
+
+from keras.layers import Embedding
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+from keras.layers import Bidirectional
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import recall_score
+
+file_prefix = "/Users/davisallen/Education/Hunter/Classes/CSCI 760 - Computer Linguistics/Projects/SentiProject/Data/SemEval2017/processed/"
+
+fn1 = "twitter-2016test-A.txt"
+fn2 = "aggregate.txt"
+fn3 = "test.txt"
+
+data1 = pd.read_csv(file_prefix+fn1, header = None, delimiter="\t")
+data2 = pd.read_csv(file_prefix+fn2, header = None, delimiter="\t", quoting=3)
+data3 = pd.read_csv(file_prefix+fn3, header = None, delimiter="\t", quoting=3)
+
+d1 = data1.values
+d2 = data2.values
+d3 = data3.values
+
+nb_validation_samples = int(d3.shape[0])
+
+tw1 = d1[:,2]
+tw2 = d2[:,2]
+tw3 = d3[:,2]
+
+c1 = d1[:,1]
+c2 = d2[:,1]
+c3 = d3[:,1]
+
+print(np.unique(c1))
+print(np.unique(c2))
+print(np.unique(c3))
+
+tweets_array = np.concatenate((tw1,tw2,tw3), axis=0)
+class_array_raw = np.concatenate((c1,c2,c3), axis=0)
+
+encoder = LabelEncoder()
+encoder.fit(class_array_raw)
+class_array = encoder.transform(class_array_raw)
+deep_class_array = np_utils.to_categorical(class_array)
+
+
+
+
+
+print(datetime.datetime.now())
+print("Tokenizing and counting")
+# Create matrix of size num_reviews x num_tokens with counts for each word
+vectorizer = CountVectorizer(min_df=2, tokenizer=nltk.word_tokenize)
+tweets_word_counts = vectorizer.fit_transform(tweets_array)
+print(datetime.datetime.now())
+
+# Create TF IDF matrix
+tfidf_transformer = TfidfTransformer()
+tweets_words_tfidf = tfidf_transformer.fit_transform(tweets_word_counts)
+
+
+
+
+# max_length = 67
+max_length = 140
+
+
+print(datetime.datetime.now())
+
+print("Tokenizing and indexing")
+# only use the top 20000 most commonly occurring words
+tokenizer = Tokenizer(nb_words=20000)
+# tokenize the reviews and generate reviews that are lists of indices for tokens in the dictionary
+tokenizer.fit_on_texts(tweets_array)
+sequences = tokenizer.texts_to_sequences(tweets_array)
+# the dictionary of tokens to indices
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+data = pad_sequences(sequences, maxlen=max_length)
+
+
+
+
+
+deep_x_train = data[:-nb_validation_samples]
+deep_x_test = data[-nb_validation_samples:]
+
+counts_x_train = tweets_word_counts[:-nb_validation_samples]
+counts_x_test = tweets_word_counts[-nb_validation_samples:]
+
+tfidf_x_train = tweets_words_tfidf[:-nb_validation_samples]
+tfidf_x_test = tweets_words_tfidf[-nb_validation_samples:]
+
+y_train = class_array[:-nb_validation_samples]
+y_test = class_array[-nb_validation_samples:]
+
+# deep_y_train and deep_y_test
+deep_y_train = deep_class_array[:-nb_validation_samples]
+deep_y_test = deep_class_array[-nb_validation_samples:]
+
+
+
+
+
+print("\n\n")
+
+print("Training NB classifier with counts")
+nb_model1 = MultinomialNB().fit(counts_x_train, y_train)
+# Predicting the Test set results, find accuracy
+y_pred1 = nb_model1.predict(counts_x_test)
+print("Accuracy: %.2f%%" % (sklearn.metrics.accuracy_score(y_test, y_pred1)*100))
+# print("Report:\n")
+# report = classification_report(y_test, y_pred1)
+# print(report)
+print("Recall:")
+recall = recall_score(y_test, y_pred1, average='macro')
+print(recall)
+
+
+
+print("\n\n")
+
+
+print("Training NB classifier with TF-IDF")
+nb_model2 = MultinomialNB().fit(tfidf_x_train, y_train)
+# Predicting the Test set results, find accuracy
+y_pred2 = nb_model1.predict(tfidf_x_test)
+print("Accuracy: %.2f%%" % (sklearn.metrics.accuracy_score(y_test, y_pred2)*100))
+# print("Report:\n")
+# report = classification_report(y_test, y_pred1)
+# print(report)
+print("Recall:")
+recall = recall_score(y_test, y_pred2, average='macro')
+print(recall)
+
+print("\n\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# deep_y_train and deep_y_test
+
+print(datetime.datetime.now())
+
+print("Building LSTM with no initial embedding vector weights")
+print("Vector length is 200")
+embedding_vector_length = 200
+model1 = Sequential()
+model1.add(Embedding(len(word_index) + 1, embedding_vector_length, input_length=max_length))
+model1.add(LSTM(100))
+model1.add(Dense(3, activation='softmax'))
+model1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model1.summary())
+model1.fit(deep_x_train, deep_y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores1 = model1.evaluate(deep_x_test, deep_y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores1[1]*100))
+print("\n\n")
+pred1 = model1.predict(deep_x_test, batch_size=32, verbose=1)
+predicted1 = np.argmax(pred1, axis=1)
+print("Recall:")
+recall = recall_score(y_test, predicted1, average='macro')
+print(recall)
+
+
+
+
+
+print(datetime.datetime.now())
+
+print("Loading GloVe pretrained word vectors")
+print("This may take some time")
+from gensim.models import KeyedVectors
+en_model = KeyedVectors.load_word2vec_format('/Users/davisallen/Education/Hunter/Classes/CSCI 760 - Computer Linguistics/Projects/SentiProject/Data/GloveTwitterVectors/glove.twitter.27B.200d.w2vformat.txt')
+
+embedding_matrix = np.zeros((len(word_index) + 1, 200))
+for word, i in word_index.items():
+    if word in en_model:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = en_model[word]
+
+
+
+print(datetime.datetime.now())
+
+print("Building LSTM using fasttext pretrained vectors")
+print("Vector length is 200")
+print("We are keeping the word vectors static and not allowing them to be modified during training")
+model2 = Sequential()
+model2.add(Embedding(len(word_index) + 1, 200, weights=[embedding_matrix], input_length=max_length, trainable=False))
+model2.add(LSTM(100))
+model2.add(Dense(3, activation='softmax'))
+model2.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model2.summary())
+model2.fit(deep_x_train, deep_y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores2 = model2.evaluate(deep_x_test, deep_y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores2[1]*100))
+print("\n\n")
+pred2 = model2.predict(deep_x_test, batch_size=32, verbose=1)
+predicted2 = np.argmax(pred2, axis=1)
+print("Recall:")
+recall = recall_score(y_test, predicted2, average='macro')
+print(recall)
+
+
+
+
+
+
+print(datetime.datetime.now())
+
+print("Building LSTM using fasttext pretrained vectors")
+print("Vector length is 200")
+print("Word vectors can be modified during training")
+model3 = Sequential()
+model3.add(Embedding(len(word_index) + 1, 200, weights=[embedding_matrix], input_length=max_length))
+model3.add(LSTM(100))
+model3.add(Dense(3, activation='softmax'))
+model3.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model3.summary())
+model3.fit(deep_x_train, deep_y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores3 = model3.evaluate(deep_x_test, deep_y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores3[1]*100))
+print("\n\n")
+pred3 = model3.predict(deep_x_test, batch_size=32, verbose=1)
+predicted3 = np.argmax(pred3, axis=1)
+print("Recall:")
+recall = recall_score(y_test, predicted3, average='macro')
+print(recall)
+
+
+
+
+
+
+print(datetime.datetime.now())
+
+embedding_vector_length = 200
+print("Building BIDIRECTIONAL LSTM using fasttext pretrained vectors")
+print("Vector length is 200")
+print("Word vectors can be modified during training")
+model3 = Sequential()
+model3.add(Embedding(len(word_index) + 1, 200, weights=[embedding_matrix], input_length=max_length))
+# model3.add(Embedding(len(word_index) + 1, embedding_vector_length, input_length=max_length))
+model3.add(Bidirectional(LSTM(100), merge_mode='concat'))
+model3.add(Dense(3, activation='softmax'))
+model3.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model3.summary())
+model3.fit(deep_x_train, deep_y_train, nb_epoch=3, batch_size=64)
+# Final evaluation of the model
+scores3 = model3.evaluate(deep_x_test, deep_y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores3[1]*100))
+print("\n\n")
+pred3 = model3.predict(deep_x_test, batch_size=32, verbose=1)
+predicted3 = np.argmax(pred3, axis=1)
+print("Recall:")
+recall = recall_score(y_test, predicted3, average='macro')
+print(recall)
+
+
+
+
+
+
+# End
